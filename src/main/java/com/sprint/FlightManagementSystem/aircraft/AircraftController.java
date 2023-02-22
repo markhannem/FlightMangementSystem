4e7c3448d24709adbb43b9add20baa25980898f7
Mark <markhannem3@gmail.com>
2023-02-21 23:58:15 -0330

    Updated controllers for CRUD requests Error when updating Airports: creates a new airport data

--- src/main/java/com/sprint/FlightManagementSystem/aircraft/AircraftController.java
+++ src/main/java/com/sprint/FlightManagementSystem/aircraft/AircraftController.java
@@ -1,11 +1,13 @@
 package com.sprint.FlightManagementSystem.aircraft;
 
-import com.sprint.FlightManagementSystem.airport.Airport;
-import com.sprint.FlightManagementSystem.airport.AirportRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/aircraft")
@@ -15,32 +17,49 @@ public class AircraftController {
     private AircraftRepository aircraftRepository;
 
     // Get all aircraft
-    @GetMapping("/")
+    @GetMapping("/aircrafts")
     public List<Aircraft> getAllAircraft() {
         return (List<Aircraft>) aircraftRepository.findAll();
     }
 
     // Get aircraft by id
-    @GetMapping("/{id}")
+    @GetMapping("/aircraft/{id}")
     public Aircraft getAircraftById(@PathVariable Long id) {
         return aircraftRepository.findById(id).get();
     }
 
     // Create a new aircraft
-    @PostMapping("/")
+    @PostMapping("/aircraft")
     public void createAircraft(@RequestBody Aircraft aircraft) {
         aircraftRepository.save(aircraft);
     }
 
     // Update an existing aircraft
-    @PutMapping("/{id}")
-    public void updateAircraft(@PathVariable Long id, @RequestBody Aircraft aircraft) {
-        aircraftRepository.save(aircraft);
-    }
+    @PutMapping("/aircraft/{id}")
+    public void updateAircraft(@PathVariable String id, @RequestBody Aircraft aircraft,
+                               HttpServletResponse response) {
+        Optional<Aircraft> returnValue = aircraftRepository.findById(Long.parseLong(id));
+        Aircraft acToUpdate;
 
+
+        if (returnValue.isPresent()) {
+            acToUpdate = returnValue.get();
+
+            acToUpdate.setType(aircraft.getType());
+
+
+            aircraftRepository.save(aircraft);
+        } else {
+            try {
+                response.sendError(404, "Aircraft with id: " + aircraft.getId() + " not found.");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
     // Delete an existing aircraft
-    @DeleteMapping("/{id}")
-    public void deleteAircraft(@PathVariable Long id) {
+    @DeleteMapping("/aircraft/{id}")
+    public void deleteAircraft (@PathVariable Long id){
         aircraftRepository.deleteById(id);
     }
 }
---
+++ src/main/java/com/sprint/FlightManagementSystem/passenger/PassengerController.java
@@ -0,0 +1,53 @@
+package com.sprint.FlightManagementSystem.passenger;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+public class PassengerController {
+
+    @Autowired
+    private PassengerRepository repo;
+
+    @GetMapping("/passengers")
+    public List<Passenger> getAllPassengers() {return (List<Passenger>) repo.findAll();}
+
+    @PostMapping("/passenger")
+    public  String createPassenger(@RequestBody Passenger passenger) {
+        repo.save(passenger);
+        return "Passenger Added";
+
+    }
+
+    @PutMapping("/passenger/{id}")
+    public void updatePassenger(@PathVariable String id, @RequestBody Passenger passenger, HttpServletResponse response) {
+        Optional<Passenger> returnValue = repo.findById(Long.parseLong(id));
+        Passenger passengerToUpdate;
+
+        if (returnValue.isPresent()) {
+            passengerToUpdate = returnValue.get();
+
+            passengerToUpdate.setFirstName(passenger.getFirstName());
+            passengerToUpdate.setLastName(passenger.getLastName());
+            passengerToUpdate.setPhoneNumber(passenger.getPhoneNumber());
+
+            repo.save(passengerToUpdate);
+        } else {
+            try {
+                response.sendError(404, "Passenger with id: " + passenger.getId() + " not found.");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
+
+
+
