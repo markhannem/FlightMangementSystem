5086999c8c7feb862919c92340dd86e0e6c67928
Chris Pinto <Chris.pinto1234@gmail.com>
2023-02-22 12:28:03 -0330

    Added All Test Files

--- src/test/java/com/sprint/FlightManagementSystem/PassengerControllerTest.java
+++ src/test/java/com/sprint/FlightManagementSystem/PassengerControllerTest.java
@@ -4,8 +4,9 @@ import com.sprint.FlightManagementSystem.airport.Airport;
 import com.sprint.FlightManagementSystem.airport.AirportRepository;
 import com.sprint.FlightManagementSystem.city.City;
 import com.sprint.FlightManagementSystem.city.CityRepository;
+import com.sprint.FlightManagementSystem.passenger.PassengerRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,8 +31,33 @@ public class PassengerControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-//    @Autowired
-//    private PassengerRepository passengerRepository;
+    @Autowired
+    private PassengerRepository passengerRepository;
 
+    @Autowired
+    private AirportRepository airportRepository;
 
+    @Test
+    public void testGetAllPassengers() throws Exception {
+        MvcResult result = mockMvc.perform(get("/passengers")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        List<City> cityList = new ArrayList<>();
+
+        String content = result.getResponse().getContentAsString();
+        System.out.println(content);
+    }
+
+    @Test
+    public void testGetPassengerById() throws Exception {
+        MvcResult result = mockMvc.perform(get("/passengers/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        List<City> cityList = new ArrayList<>();
+
+        String content = result.getResponse().getContentAsString();
+        System.out.println(content);
+    }
 }
