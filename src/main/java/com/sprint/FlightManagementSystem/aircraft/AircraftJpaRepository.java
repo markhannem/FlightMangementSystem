c0878cfe9dc4fb9aa9a127d4233fbf60f2911d4f
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2023-02-27 10:36:02 -0330

    Added @Repository

--- src/main/java/com/sprint/FlightManagementSystem/aircraft/AircraftJpaRepository.java
+++ src/main/java/com/sprint/FlightManagementSystem/aircraft/AircraftJpaRepository.java
@@ -1,9 +1,11 @@
 package com.sprint.FlightManagementSystem.aircraft;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 // save(), findOne(), findById(), findAll(), count(), delete(), deleteById(), etc...
 public interface AircraftJpaRepository extends JpaRepository<Aircraft, Long> {
     List<Aircraft> findAircratByPassengerId(Long passenger_id);
